d2d0a4a3b6078d91d1bc8e8a16719b640d252fc3
KaraboMalehuMolepo <molepok@mogaleintergrated.co.za>
2026-01-27 10:20:05 +0200

    Fix CORS: Add http://10.0.0.109:3004 and localhost:3004 to allowed origins

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,17 @@ app.use(cors({
     'http://localhost:3000', 
     'http://localhost:3001',
     'http://localhost:3002',
+    'http://localhost:3004',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
     'http://127.0.0.1:3002',
+    'http://127.0.0.1:3004',
     'http://10.0.0.86:3014',
     'http://10.0.0.86:3013',
     'http://10.0.0.109:3000',
     'http://10.0.0.109:3001',
-    'http://10.0.0.109:3002'
+    'http://10.0.0.109:3002',
+    'http://10.0.0.109:3004'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
